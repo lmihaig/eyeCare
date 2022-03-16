160c05541b40cbc7dc6f504961d34597d3aed0a3
Licu Mihai <licu.mihai21@gmail.com>
2022-03-16 18:27:34 +0200

    implemented uploadImage

--- eyeCare_frontend/lib/main.dart
+++ eyeCare_frontend/lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,14 +37,35 @@ class _MainPageState extends State<MainPage> {
 
   Future chooseImage(ImageSource source) async {
     try {
-      final image = await ImagePicker().pickImage(source: source);
+      final image = await ImagePicker()
+          .pickImage(source: source, preferredCameraDevice: CameraDevice.front);
       if (image == null) return;
 
       final imageTemp = File(image.path);
       setState(() => this.image = imageTemp);
+      uploadImage(this.image);
     } on PlatformException catch (e) {
       print("Failed to choose image: $e");
     }
+  }
+
+  Future uploadImage(File? image) async {
+    var uri = Uri.parse('http://sima.zapto.org:8081/api/add_job');
+    Map<String, String> headers = {"user": "muie_sima"};
+
+    var request = http.MultipartRequest("POST", uri);
+    request.headers.addAll(headers);
+
+    request.fields["parola"] = "sima_muie";
+
+    request.files.add(http.MultipartFile.fromBytes(
+        "file", image!.readAsBytesSync(),
+        filename: "Photo.jpg", contentType: MediaType("image", "jpg")));
+
+    http.Response response =
+        await http.Response.fromStream(await request.send());
+    print("Result: ${response.statusCode}");
+    print("Body: ${response.body}");
   }
 
   Widget fancyButton(
@@ -70,30 +93,32 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 60, 175, 195),
+      backgroundColor: const Color.fromARGB(255, 60, 175, 195),
       body: Container(
         padding: const EdgeInsets.all(32),
         child: Column(
           children: [
-            image != null
-                ? Image.file(
-                    image!,
-                    // width: 300,
-                    // height: 300,
-                    fit: BoxFit.cover,
-                  )
-                : const FlutterLogo(size: 300),
-            const SizedBox(
-              height: 24,
-            ),
-            const Text(
-              'AI CANCER LA OCHI',
-              style: TextStyle(
-                fontSize: 34,
-                fontWeight: FontWeight.bold,
+            if (image != null) ...[
+              Image.file(
+                image!,
+                width: MediaQuery.of(context).size.width * 0.7,
+                height: MediaQuery.of(context).size.height * 0.7,
+                fit: BoxFit.cover,
               ),
-            ),
-            Spacer(),
+              const SizedBox(
+                height: 24,
+              ),
+              const Text(
+                'AI CANCER LA OCHI',
+                style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ] else ...[
+              const FlutterLogo(size: 300),
+            ],
+            const Spacer(),
             fancyButton(
                 title: "Take picture",
                 icon: Icons.camera_alt_outlined,
